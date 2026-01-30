3256014a465594f86caaf98ce599af4745e49d0d
Rahul Kumar Jha <rjha8340@gmail.com>
2026-01-31 00:35:11 +0530

    Added Table with sorting

--- src/app/Routes/public.routes.tsx
+++ src/app/Routes/public.routes.tsx
@@ -1,7 +1,9 @@
 import Home from "@/Page/Home/Home";
 import About from "@/Page/Home/About";
+import Table from  "@/Page/Table/index.js"
 
 export const publicRoutes = [
   { path: "/", element: <Home /> },
   { path: "/about", element: <About /> },
+  { path: '/table', element: <Table />}
 ];
